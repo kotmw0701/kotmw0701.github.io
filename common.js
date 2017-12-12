1366bc836a533484b394cf94601d2434fcb62931
motlof <supurasyu@hotmail.co.jp>
2017-12-12 23:16:37 +0900

    commit 1.0.17_4

---
+++ common.js
@@ -0,0 +1,39 @@
+﻿function header(rootDir){
+	if(rootDir == null) rootDir = "./";
+	$.ajax({
+		url: rootDir + "header.html",
+		cache: false,
+	        async: false,
+	        dataType: 'html',
+		success: function(html){
+            		document.write(html);
+		}
+	});
+}
+
+function sidebar(rootDir){
+	if(rootDir == null) rootDir = "./";
+	$.ajax({
+		url: rootDir + "sidebar.html",
+		cache: false,
+	        async: false,
+	        dataType: 'html',
+		success: function(html){
+			html = html.replace(/\{\$root\}/g, rootDir); //footer.htmlの{$root}を置換
+            		document.write(html);
+		}
+	});
+}
+
+function footer(rootDir){
+	if(rootDir == null) rootDir = "./";
+	$.ajax({
+		url: rootDir + "footer.html",
+		cache: false,
+	        async: false,
+	        dataType: 'html',
+		success: function(html){
+            		document.write(html);
+		}
+	});
+}
