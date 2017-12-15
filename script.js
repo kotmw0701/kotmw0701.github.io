8f6b8acaba6b4917fc843a12003baed57d3f9286
motlof <supurasyu@hotmail.co.jp>
2017-12-16 00:03:17 +0900

    commit 1.0.20

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿function writedate() {
+	var date = new Date();
+	
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+	
+	document.write(hour+"時"+minute+"分"+second+"秒");
+}
