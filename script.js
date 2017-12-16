57ad3696cf7bc48af5bff1f5f7dbba289d0a567b
motlof <supurasyu@hotmail.co.jp>
2017-12-16 23:55:03 +0900

    commit 1.0.20_2

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-﻿function writedate() {
+﻿function switchingStyles() {
 	var date = new Date();
-	
 	var hour = date.getHours();
-	var minute = date.getMinutes();
-	var second = date.getSeconds();
 	
-	document.write(hour+"時"+minute+"分"+second+"秒");
+	var element = document.getElementById('toggle');
+	var hrefparam = element.href;
+	
+	var css = (hour <= 6 || hour >= 19) ? hrefparam.replace("day", "night") : hrefparam;
+	
+	document.getElementById('toggle').href = css;
 }
