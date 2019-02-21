2d5f5b453dfbb71359101d85f29c86d56107c7ff
kotmw0701 <supurasyu@hotmail.co.jp>
2019-02-21 23:40:41 +0900

    commit 2.1.9 : pugからの生成に移行，cssをパーツ分け

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('pug', () => {
         .pipe(data((file) => {
             // metaのjsonファイルを取得 requireでも良かったけどそうするとキャッシュが残っちゃうからJson.parseで
 			let metaData = JSON.parse(fs.readFileSync('./_pug/data/meta.json', 'utf8', (err, data) => {
-				if(err) throw err; 
+				if(err) throw err;
 				console.log(data);
 			}));
             // 対象のファイルパスを取得して、\を/に置換
@@ -21,16 +21,15 @@ gulp.task('pug', () => {
             // jsonファイルから対象ページの情報を取得して返す
 			let global = metaData["global"];//全体の共通データを取得
 			let meta = metaData[fileName];//変更点のデータを取得
-
-			//css,jsのパスを指定するため
+			//css,jsのパスを指定するためrootまでの相対パスを生成
 			for (let i = 0; i < fileName.slice(1).split("/").length-1; i++) {
 				global["root"] += "../";
-			}
+            }
+            //テンプレートとの変更点を変更する
             Object.keys(meta).forEach(function(key) {
                 global[key] = this[key];
 			}, meta);
 			
-			// console.log(meta);
             return global;
         }))
         .pipe(pug())
@@ -38,5 +37,5 @@ gulp.task('pug', () => {
             indent_size: 1,
             indent_char: '\t'
         }))
-        .pipe(gulp.dest('./output/'));
+        .pipe(gulp.dest('./'));
 });
