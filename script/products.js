606079777f32d19fb186f97685d8b27fd37284c3
kotmw0701 <supurasyu@hotmail.co.jp>
2019-02-25 21:20:34 +0900

    commit 2.1.16 : クラスの付け忘れを修正，productページのCSSの修正

--- script/products.js
+++ script/products.js
@@ -1,7 +1,7 @@
 $(function() {
     $(".products .content").each(function(i) {
         setTimeout(function() {
-            $(".products .content").eq(i).addClass("active");
+            $(".products .inner").eq(i).addClass("active");
         }, 150 * i);
     });
 })
